5a4c74559459d25d92a562bfb6716592f1d596c7
Maurice Fiorani <mauricef97@gmail.com>
2020-02-04 02:00:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,8 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
  
- holiday_hash.each do |
+ holiday_hash.each do |season, holiday|
+   
  
  
   # iterate through holiday_hash and print items such that your readout resembles:
