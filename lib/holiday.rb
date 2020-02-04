b8f80b362baf85cc524d42f2ddb547920a029249
Maurice Fiorani <mauricef97@gmail.com>
2020-02-04 02:02:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
  
  holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}"
-  season.each do |holi
+  holiday.each do 
  
  
   # iterate through holiday_hash and print items such that your readout resembles:
