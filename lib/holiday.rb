137ec46c9062774fb8c42769a824a49af01a1af7
Maurice Fiorani <mauricef97@gmail.com>
2020-02-04 02:19:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,9 +96,10 @@ def all_holidays_with_bbq(holiday_hash)
       value.each do |supply|
         if supply == "BBQ"
           answer.push(holiday)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+      end
+    end  
 end
 end
+answer
 
+end
